8449b69284be2a8ca5efe49cdee49c852b160a00
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2024-03-27 18:31:25 -0700

    basic parsing complete

---
+++ scibowl.js
@@ -0,0 +1,93 @@
+let questionInput = document.getElementById("questionInput");
+
+let text = `TOSS-UP
+2) CHEMISTRY Multiple Choice An aqueous solution in which the concentration of OH– ions is
+greater than that of H+ ions is:
+W) basic
+X) acidic
+Y) neutral
+Z) in equilibrium
+ANSWER: W) BASIC
+BONUS
+2) CHEMISTRY Short Answer Find the mass of 1 mole of cuprous oxide, or Cu2O. Assume the
+atomic mass of copper is 64 and oxygen is 16.
+ANSWER: 144`;
+
+function processQuestions() {
+  let questions = questionInput.value;
+
+  console.log(parseQuestion(questions));
+}
+
+function parseQuestion(text) {
+  let questionGroup = {};
+
+  let temp = text.split("BONUS");
+  let [rawTossUp, rawBonus] = [temp[0], temp[1]];
+  let [tossUpText, bonusText] = [temp[0].split("\n"), temp[1].split("\n")];
+
+  console.log(tossUpText);
+  console.log(bonusText);
+
+  console.log(rawTossUp);
+  console.log(rawBonus);
+
+  //Subject
+  temp = tossUpText[1].split(" ");
+
+  questionGroup.subject = temp[1];
+  console.log(questionGroup.subject);
+
+  //Toss Up
+  let tossUp = {};
+
+  tossUp.questionType =
+    temp[2] == "Multiple" ? "Multiple Choice" : "Short Answer";
+  console.log(tossUp.questionType);
+
+  //Question
+  let questionStart =
+    rawTossUp.indexOf(tossUp.questionType) + tossUp.questionType.length;
+  let questionEnd = rawTossUp.indexOf("ANSWER");
+
+  tossUp.question = rawTossUp.substring(questionStart, questionEnd).trim();
+
+  console.log(tossUp.question);
+
+  //Answer
+  tossUp.answer = rawTossUp
+    .substring(rawTossUp.indexOf("ANSWER:"))
+    .replace("ANSWER:", "")
+    .trim();
+  console.log(tossUp.answer);
+
+  //Bonus
+  temp = bonusText[1].split(" ");
+
+  let bonus = {};
+
+  bonus.questionType =
+    temp[2] == "Multiple" ? "Multiple Choice" : "Short Answer";
+  console.log(bonus.questionType);
+
+  //Question
+  questionStart =
+    rawBonus.indexOf(bonus.questionType) + bonus.questionType.length;
+  questionEnd = rawBonus.indexOf("ANSWER");
+
+  bonus.question = rawBonus.substring(questionStart, questionEnd).trim();
+
+  console.log(bonus.question);
+
+  //Bonus
+  bonus.answer = rawBonus
+    .substring(rawBonus.indexOf("ANSWER:"))
+    .replace("ANSWER:", "")
+    .trim();
+  console.log(bonus.answer);
+
+  questionGroup.tossUp = tossUp;
+  questionGroup.bonus = bonus;
+
+  return questionGroup;
+}
