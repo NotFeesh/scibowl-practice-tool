3e7214460444d23714a48d0da5242c0441df5d14
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2024-03-27 19:41:07 -0700

    started working on actual question functionality

--- scibowl.js
+++ scibowl.js
@@ -1,105 +1,11 @@
-let questionInput = document.getElementById("questionInput");
-let questionInputLabel = document.getElementById("questionInputLabel");
-let sizeDisplay = document.getElementById("numberQuestions");
+let questions = JSON.parse(sessionStorage.getItem("questionList"));
+let currentQuestion = sessionStorage.getItem("currentQuestion")
+  ? sessionStorage.getItem("currentQuestion")
+  : 0;
 
-let questionList = JSON.parse(sessionStorage.getItem("questionList"));
-updateDisplay();
+console.log(currentQuestion);
 
-function updateDisplay() {
-  sizeDisplay.textContent = `Current Question Set Size: ${questionList.length} Questions`;
-}
-
-function clearSet() {
-  questionList = [];
-  updateDisplay();
-}
-
-function processQuestions() {
-  let rawQuestions = questionInput.value;
-  let questions = rawQuestions.split("TOSS-UP");
-  for (let i = 1; i < questions.length; i++) {
-    questionList.push(parseQuestion(questions[i]));
-  }
-  sessionStorage.setItem("questionList", JSON.stringify(questionList)); //Pass to the next page
-  questionInput.value = "";
-  questionInputLabel.textContent =
-    "Question Set Successfully Submitted! Please go to the Questions tab to practice!";
-  updateDisplay();
-}
-
-function parseQuestion(text) {
-  let questionGroup = {};
-
-  let temp = text.split("BONUS");
-  let [rawTossUp, rawBonus] = [temp[0], temp[1]];
-  let [tossUpText, bonusText] = [temp[0].split("\n"), temp[1].split("\n")];
-
-  /*
-  console.log(tossUpText);
-  console.log(bonusText);
-
-  console.log(rawTossUp);
-  console.log(rawBonus);
-  */
-
-  //Subject
-  temp = tossUpText[1].split(" ");
-
-  questionGroup.subject = temp[1];
-  console.log(questionGroup.subject);
-
-  //Toss Up
-  let tossUp = {};
-
-  tossUp.questionType =
-    temp[2] == "Multiple" ? "Multiple Choice" : "Short Answer";
-  console.log(tossUp.questionType);
-
-  //Question
-  let questionStart =
-    rawTossUp.indexOf(tossUp.questionType) + tossUp.questionType.length;
-  let questionEnd = rawTossUp.indexOf("ANSWER");
-
-  tossUp.question = rawTossUp.substring(questionStart, questionEnd).trim();
-
-  console.log(tossUp.question);
-
-  //Answer
-  temp = rawTossUp
-    .substring(rawTossUp.indexOf("ANSWER:"))
-    .replace("ANSWER:", "")
-    .split("\n");
-  tossUp.answer = temp[0].trim();
-  console.log(tossUp.answer);
-
-  //Bonus
-  temp = bonusText[1].split(" ");
-
-  let bonus = {};
-
-  bonus.questionType =
-    temp[2] == "Multiple" ? "Multiple Choice" : "Short Answer";
-  console.log(bonus.questionType);
-
-  //Question
-  questionStart =
-    rawBonus.indexOf(bonus.questionType) + bonus.questionType.length;
-  questionEnd = rawBonus.indexOf("ANSWER");
-
-  bonus.question = rawBonus.substring(questionStart, questionEnd).trim();
-
-  console.log(bonus.question);
-
-  //Answer
-  temp = rawBonus
-    .substring(rawBonus.indexOf("ANSWER:"))
-    .replace("ANSWER:", "")
-    .split("\n");
-  bonus.answer = temp[0].trim();
-  console.log(bonus.answer);
-
-  questionGroup.tossUp = tossUp;
-  questionGroup.bonus = bonus;
-
-  return questionGroup;
+function newQuestion() {
+  console.log(questions[currentQuestion]);
+  currentQuestion++;
 }
